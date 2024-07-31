4e3f9f193bf08ebf2124353cae79d84c5d101ed0
Carlos-marinho <carloslucas.tavares@gmail.com>
2024-07-31 12:09:30 -0300

    Add Login Function with JWT Token

--- routes/public.js
+++ routes/public.js
@@ -1,23 +1,71 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 const prisma = new PrismaClient();
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 router.post("/register", async (req, res) => {
-    const { username , password } = req.body;
+    const { username, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    const userDB = await prisma.user.create({
-        data: {
-            username: username,
-            password: hashedPassword,
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            username: username
         }
     })
 
-    res.status(201).json(userDB)
-})
+    if (existingUser) {
+        res.status(400).json({
+            sucess: false,
+            message: "User already taken"
+        })
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const userDB = await prisma.user.create({
+        data: {
+        username: username,
+        password: hashedPassword,
+        },
+    });
+
+    res.status(201).json(userDB);
+});
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    });
+
+    if (!user) {
+        return res.status(401).json({message: "User not found"})
+    };
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (isPasswordMatch) {
+        const token = jwt.sign({
+            username: user.username,
+            id: user.id
+        },JWT_SECRET)
+
+        res.status(200).json({
+            sucess: true,
+            token: token
+        })
+    } else {
+        res.status(400).json({
+            message: "Incorrect Password",
+            sucess: false
+        })
+    }
+});
 
 export default router;
